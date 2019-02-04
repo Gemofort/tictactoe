f1ea1c00f5dbe705041f2377b093cd2b81c157aa
Gemofort <vanya6677@gmail.com>
2019-02-04 22:38:37 +0200

    Nearly finall change

--- src/Names.jsx
+++ src/Names.jsx
@@ -7,57 +7,69 @@ class Names extends Component {
     super(props);
 
     this.state = {
-      name1: '',
-      name2: '',
-      names: ['', '']
-    };
-
-    this.handleChangeFirst = this.handleChangeFirst.bind(this);
-    this.handleChangeSecond = this.handleChangeSecond.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+      name1: null,
+      name2: null,
+      names: [null, null],
+      submitted: false
+    }
   }
 
-  handleChangeFirst(event) {
+  handleChangeFirst = (event) => {
     this.setState({
       name1: event.target.value,
       name2: this.state.name2
     });
   }
 
-  handleChangeSecond(event) {
+  handleChangeSecond = (event) => {
     this.setState({
       name1: this.state.name1,
       name2: event.target.value
     });
   }
 
-  handleSubmit(event) {
+  handleSubmit = (event) => {
     event.preventDefault();
     let updNames = Array(2);
     updNames[0] = this.state.name1;
     updNames[1] = this.state.name2;
-    this.setState({
-      name1: this.state.name1,
-      name2: this.state.name2,
-      names: updNames
-    });
+    if (updNames[0] && updNames[1] && updNames[0] !== updNames[1]) {
+      this.setState({
+        name1: this.state.name1,
+        name2: this.state.name2,
+        names: updNames,
+        submitted: !this.state.submitted
+      });
+    }
+  }
+
+  showApp = () => {
+    if (this.state.submitted) {
+      return (
+        <App
+          name1={this.state.names[0]}
+          name2={this.state.names[1]}
+        />);
+    } else {
+      return (
+        <React.Fragment>
+          <form className='playerNames' onSubmit={this.handleSubmit}>
+            <h1> <b>Tic Tac Toe</b> by Ivan Feofanov</h1>
+            <h3>Please enter your nicknames(at least 1 letter and not the same)</h3>
+            First Player:<br />
+            <input type='text' name1={this.state.name1} onChange={this.handleChangeFirst} /> <br />
+            Second Player: <br />
+            <input type='text' name2={this.state.name1} onChange={this.handleChangeSecond} /><br />
+            <input type='submit' value='Submit names' />
+          </form>
+        </React.Fragment>
+      );
+    }
   }
 
   render() {
     return (
-      <React.Fragment>
-        <form className='playerNames' onSubmit={this.handleSubmit}>
-          First Player:<br />
-          <input type='text' name1={this.state.name1} onChange={this.handleChangeFirst} /> <br />
-          Second Player: <br />
-          <input type='text' name2={this.state.name1} onChange={this.handleChangeSecond} /><br />
-          <input type='submit' value='Submit names' />
-        </form>
-        <App
-          name1={this.state.names[0]}
-          name2={this.state.names[1]}
-        />
-      </React.Fragment>
+      this.showApp()
     );
   }
 }
